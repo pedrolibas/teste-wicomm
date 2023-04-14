fff6855a577234c84490653f45da6ff1946f2d7f
Pedro Gabriel <pedrogv03@hotmail.com>
2023-04-14 16:14:29 -0300

    feat: add style guide and reset css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { GlobalStyle } from "./styles/global";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
