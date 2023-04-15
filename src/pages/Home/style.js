78f0bd824dea001cb7f6ba87a245aeb3fd4825a1
Pedro Gabriel <pedrogv03@hotmail.com>
2023-04-14 23:26:43 -0300

    feat: list products created

--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const ContainerHome = styled.div`
   width: 100vw;
   height: 100vh;
+
+  .list-type{
+    margin-top: 56px;
+  }
 `;
---
+++ src/utils/data.js
@@ -0,0 +1,10 @@
+import imgTypeProduct from "../assets/typeProduct1.png";
+
+export const dataTypeProducts = [
+  { img: imgTypeProduct, name: "PROTEÍNAS" },
+  { img: imgTypeProduct, name: "HIPERCALÓRICOS" },
+  { img: imgTypeProduct, name: "CREATINA" },
+  { img: imgTypeProduct, name: "PACKS" },
+  { img: imgTypeProduct, name: "AMINOÁCIDOS" },
+  { img: imgTypeProduct, name: "SAÚDE" },
+];
