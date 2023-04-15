14d971b40355f1b59d318cfea8498c9f8de2e4ca
Pedro Gabriel <pedrogv03@hotmail.com>
2023-04-15 01:08:30 -0300

    feat: finished

---
+++ src/components/CarouselPeople/style.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const ContainerCarouselPeople = styled.section`
+  width: 100%;
+  height: 461px;
+  position: relative;
+
+  .background {
+    width: inherit;
+    height: inherit;
+    position: absolute;
+  }
+
+  h2 {
+    font-style: italic;
+    color: var(--white);
+
+    position: absolute;
+    top: 40px;
+    left: 205px;
+  }
+
+  strong {
+    color: var(--orange1);
+  }
+
+  ul {
+    width: calc(100% - 205px - 205px);
+
+    padding: 0 205px;
+
+    position: absolute;
+    top: 111px;
+
+    .left {
+      left: 141px;
+    }
+
+    .right {
+      right: 141px;
+    }
+    li {
+      img {
+        width: 267px;
+        height: 284px;
+      }
+    }
+  }
+
+  .redirect {
+    color: var(--black1);
+
+    padding: 14px 24px;
+
+    background-color: var(--orange1);
+
+    border: none;
+    border-radius: 4px;
+
+    position: relative;
+    left: 571px;
+    top: 437px;
+  }
+`;
