0f9dfc4f2244e4f044229f541dc39759500305dc
Pedro Gabriel <pedrogv03@hotmail.com>
2023-04-14 22:46:27 -0300

    feat: circle card created

---
+++ src/components/CircleCard/style.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const ContainerCircleCard = styled.li`
+  width: 178px;
+  height: 210px;
+
+  div {
+    width: inherit;
+    height: 178px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color: var(--background);
+
+    border-radius: 50%;
+
+    img {
+      width: 108px;
+      height: 108px;
+    }
+  }
+
+  h2 {
+    text-align: center;
+
+    margin-top: 16px;
+  }
+`;
